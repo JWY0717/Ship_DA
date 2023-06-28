9fe65e8dfc8f617668bd97e76e54dec781b503d3
내 이름 <122770858+JWY0717@users.noreply.github.com>
2023-06-28 17:46:31 +0900

    06.26수정파일

--- DA/notebooks/app05.py
+++ DA/notebooks/app05.py
@@ -4,10 +4,13 @@ import pickle
 import pandas as pd
 from urllib.request import urlopen
 from datetime import datetime
-    
-app = Flask(__name__)
-CORS(app)
 
+# import flask_cors CORS, cross_orgin
+
+app = Flask(__name__)
+
+
+cors = CORS(app)
 # 모델 불러오기
 
 with open('D:/장우영/LOCALSEARCH/Ship_DA/DA/model/model_latitude.pkl', 'rb') as f:
@@ -25,7 +28,7 @@ with open('D:/장우영/LOCALSEARCH/Ship_DA/DA/model/model_sog.pkl', 'rb') as f:
 # 기상청 API URL
 weather_api_url ='https://apihub.kma.go.kr/api/typ01/url/kma_buoy.php?tm=202306161610&stn=21229&help=1&authKey=cJGQY1PQTnuRkGNT0H57zQ'
 
-@app.route('/api05/predict/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
     data = request.get_json()
     app.logger.info(data)
@@ -51,37 +54,45 @@ def predict():
     second = now.second
           
     # 위도와 경도를 기반으로 기상 데이터를 기상청 API에서 가져옴
-    weather_data = get_weather_data(latitude, longitude)
+    # weather_data = get_weather_data(latitude, longitude)
+    wind_direction = 100            # 풍향
+    temperatures = 15                # 기온
+    water_temperature = 10         # 수온
+    wind_speed = 100                # 풍속
+    atmospheric_pressure = 10       # 기압
+    humidity = 10                 # 상대습도
+    direct = 10                     # 유향
+    current_speed = 10
     
 
-    if weather_data is not None:
+    # if weather_data is not None:
         
-        # 관련 기상 데이터 추출
-        wind_direction = weather_data.get('WD', 100)             # 풍향
-        temperatures = weather_data.get('TA', 15)                # 기온
-        water_temperature = weather_data.get('TW', 10)           # 수온
-        wind_speed = weather_data.get('WS', 100)                 # 풍속
-        atmospheric_pressure = weather_data.get('PA', 10)        # 기압
-        humidity = weather_data.get('HM', 10)                    # 상대습도
-        direct = weather_data.get('CD', 10)                      # 유향
-        current_speed = weather_data.get('CS', 10)               # 유속
-        
-        #X = data                       [["mmsi", "ship_type", "latitude","longitude", "cog", "sog", "year", "month", "day", "hour", "minute", "second", "풍향", "기온", "수온", "풍속", "유속", "기압", "습도"]]
-        # = data[["mmsi", "ship_type", "latitude","longitude", "cog", "sog", "year", "month", "day", "hour", "minute", "second", "풍향", "유향", "기온", "수온", "풍속", "유속", "기압", "습도"]]
+    # 관련 기상 데이터 추출
+    # wind_direction = weather_data.get('WD', 100)             # 풍향
+    # temperatures = weather_data.get('TA', 15)                # 기온
+    # water_temperature = weather_data.get('TW', 10)           # 수온
+    # wind_speed = weather_data.get('WS', 100)                 # 풍속
+    # atmospheric_pressure = weather_data.get('PA', 10)        # 기압
+    # humidity = weather_data.get('HM', 10)                    # 상대습도
+    # direct = weather_data.get('CD', 10)                      # 유향
+    # current_speed = weather_data.get('CS', 10)               # 유속
+    
+    #X = data                       [["mmsi", "ship_type", "latitude","longitude", "cog", "sog", "year", "month", "day", "hour", "minute", "second", "풍향", "기온", "수온", "풍속", "유속", "기압", "습도"]]
+    # = data[["mmsi", "ship_type", "latitude","longitude", "cog", "sog", "year", "month", "day", "hour", "minute", "second", "풍향", "유향", "기온", "수온", "풍속", "유속", "기압", "습도"]]
 
-        
-        # 모델을 사용하여 예측 수행
-        latitude = model_latitude.predict([[mmsi, ship_type, latitude,longitude,cog, sog, year, month, day, hour, minute, second,  wind_direction,direct, temperatures, water_temperature, wind_speed, current_speed,atmospheric_pressure, humidity]])
-        longitude = model_longitude.predict([[mmsi, ship_type,latitude,longitude, cog, sog, year, month, day, hour, minute, second,wind_direction,direct, temperatures, water_temperature, wind_speed, current_speed,atmospheric_pressure, humidity]])
-        cog = model_cog.predict([[mmsi, ship_type, latitude,longitude,cog, sog,year, month, day, hour, minute, second, wind_direction,direct,temperatures, water_temperature, wind_speed,  current_speed,atmospheric_pressure, humidity]])
-        sog = model_sog.predict([[mmsi, ship_type, latitude,longitude,cog, sog, year, month, day, hour, minute, second,wind_direction,direct, temperatures, water_temperature, wind_speed,  current_speed,atmospheric_pressure, humidity]])
+    
+    # 모델을 사용하여 예측 수행
+    latitude = model_latitude.predict([[mmsi, ship_type, latitude,longitude,cog, sog, year, month, day, hour, minute, second,  wind_direction,direct, temperatures, water_temperature, wind_speed, current_speed,atmospheric_pressure, humidity]])
+    longitude = model_longitude.predict([[mmsi, ship_type,latitude,longitude, cog, sog, year, month, day, hour, minute, second,wind_direction,direct, temperatures, water_temperature, wind_speed, current_speed,atmospheric_pressure, humidity]])
+    cog = model_cog.predict([[mmsi, ship_type, latitude,longitude,cog, sog,year, month, day, hour, minute, second, wind_direction,direct,temperatures, water_temperature, wind_speed,  current_speed,atmospheric_pressure, humidity]])
+    sog = model_sog.predict([[mmsi, ship_type, latitude,longitude,cog, sog, year, month, day, hour, minute, second,wind_direction,direct, temperatures, water_temperature, wind_speed,  current_speed,atmospheric_pressure, humidity]])
 
-        # 예측 결과 반환
-        result = {'latitude': latitude[0], 'longitude': longitude[0], 'cog': cog[0], 'sog': sog[0]}
-        return jsonify(result)
-    else:
-        # 기상 데이터를 가져오지 못한 경우 에러 메시지 반환
-        return jsonify({'error': '기상 데이터 가져오기 실패'})
+    # 예측 결과 반환
+    result = {'latitude': latitude[0], 'longitude': longitude[0], 'cog': cog[0], 'sog': sog[0]}
+    return result
+    # else:
+    #     # 기상 데이터를 가져오지 못한 경우 에러 메시지 반환
+    #     return jsonify({'error': '기상 데이터 가져오기 실패'})
 
 def get_weather_data(LAT, LON):
     # Construct the URL
